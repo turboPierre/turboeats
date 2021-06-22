1c4c3f6372237f3b6f3133fca0b3e7a8fd7642cc
TurboToto02 <thomaspetain2212@gmail.com>
2021-06-22 11:10:00 +0200

    front

---
+++ front_auth/src/components/HelloWorld.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group
+        id="input-group-1"
+        label="Email:"
+        label-for="input-1"
+      >
+        <b-form-input
+          id="email"
+          v-model="form.email"
+          type="email"
+          placeholder="Email"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Mot de passe:" label-for="password">
+        <b-form-input
+          id="password"
+          v-model="form.password"
+          type="password"
+          placeholder="Mot de passe"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+    </b-form>
+
+    <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        form: {
+          email: '',
+          password: '',
+        },
+        show: true
+      }
+    },
+    methods: {
+      onSubmit(event) {
+        event.preventDefault()
+        alert(JSON.stringify(this.form))
+      },
+    }
+  }
+</script>
