1078e1742fc4be1960068dd670583148a702d94f
Bate51 <baptiste.cain@laposte.net>
2021-06-25 09:46:25 +0200

    new option restaurant

--- application_API/src/models/restaurant.model.ts
+++ application_API/src/models/restaurant.model.ts
@@ -1,3 +1,4 @@
+import { POINT_CONVERSION_UNCOMPRESSED } from 'constants';
 import mongoose, {Schema, Document} from 'mongoose';
 
 export interface RestaurantInterface extends Document {
@@ -7,7 +8,13 @@ export interface RestaurantInterface extends Document {
 
 export const restaurantSchema = new Schema({
     name: { type: String, required: true },
-    _userId: {type: Number, required:true}
+    _userId: {type: Number, required:true},
+    describe: { type: String, required: false},
+    picture : { type: File, required : false},
+    adress : { type: String, required: true },
+    lunch_hours : { type: Number, required:false},
+    dinner_hours : { type: Number, required:false},
+    open_days : { type: String, required: true },
 });
 
 const Restaurant = mongoose.model<RestaurantInterface>('Restaurant', restaurantSchema);
