0dd719c6f29d94b60bbad71b1a1cbb691411a86e
turboPierre <pierre.defert@viacesi.fr>
2021-06-29 13:44:40 +0200

    Image placement restaurant

--- front_auth/src/components/PageRestaurant.vue
+++ front_auth/src/components/PageRestaurant.vue
@@ -1,29 +1,26 @@
 <template>
-  <div class="container">
-  <div class="mt-5">
-    <div class='row' style="margin-top: -10%">
+  <div class="mt-5 container">
+    <div class='mt-3'>
 
 
-      <div class="cont">
-        <img alt="restaurant" src="https://www.agencephotoup.fr/public/Medias/0_bandeaux_new/portfolio/bandeau_restaurants.jpg">
-        <div class="text-block">
-          <h4>{{ restaurant.name }}</h4>
-          <p>{{restaurant.describe}}</p>
-        </div>
+      <div class="mt-3">
+      <img style="display: block;margin:auto;" alt="restaurant" src="https://offloadmedia.feverup.com/parissecret.com/wp-content/uploads/2020/05/23132534/hero-1583762206.jpg" width="50%">
+        <h1 style="margin-top: 2%"><strong>{{ restaurant.name }}</strong></h1>
+        <p><b>{{restaurant.describe}}</b></p>
       </div>
 
 
     </div>
-    <div class="row">
+    <div class="mt-3">
       <b-tabs content-class="mt-3" fill>
         <b-tab title="Menus" active>
 
         </b-tab>
         <b-tab title="Produits">
           <div class="row">
-            <div class="col-sm-4 mt-3" v-for="product in listProducts" :key="product.id">
+            <div class="col-sm-3 mt-3" v-for="product in listProducts" :key="product.id">
               <div v-if="product._restaurantId === restaurant._id" @click="add_product(product)">
-                <h1>{{ product.name }}</h1>
+                <h3>{{ product.name }}</h3>
                 <h5>{{product.describe}}</h5>
                 <h5>{{product.price}} €</h5>
               </div>
@@ -32,7 +29,6 @@
         </b-tab>
       </b-tabs>
     </div>
-  </div>
   </div>
 </template>
 
