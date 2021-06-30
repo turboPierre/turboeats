3d9cd6f3a3ca29521feb02cd54c21012f73ad13f
TurboToto02 <thomaspetain2212@gmail.com>
2021-06-30 11:29:19 +0200

    Enregistremement en bdd commande fonctionnel

--- application_API/src/models/command.model.ts
+++ application_API/src/models/command.model.ts
@@ -7,7 +7,7 @@ export interface CommandInterface extends Document {
 export const commandSchema = new Schema({
     price: { type: String, required: true },
     _clientId: {type: Number, required:true},
-    _restaurantId: {type: Number, required:true},
+    _restaurantId: {type: String, required:true},
     _delivererId: {type: Number, required:true},
     _menuId:[
         {type: Schema.Types.ObjectId, ref: 'Menu'}
