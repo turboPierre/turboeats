e882a53ade4766ec54784c1ade4450606ca4797e
turboPierre <pierre.defert@viacesi.fr>
2021-07-01 20:01:50 +0200

    Fixed resolution

--- resolution_controller/src/controllers/command.controller.ts
+++ resolution_controller/src/controllers/command.controller.ts
@@ -23,7 +23,8 @@ export const userCommands = async(req: Request, res: Response, next: NextFunctio
 };
 
 export const restaurantCommand = async(req: Request, res: Response, next: NextFunction) => {
-    const result = await axios.get(APP_API_IP + 'commands/restaurantCommand', {
+    const itemID: string = req.params.id;
+    const result = await axios.get(APP_API_IP + 'commands/restaurantCommand' + itemID, {
         headers:req.headers
     })
         .then(response => response.data)
@@ -33,8 +34,7 @@ export const restaurantCommand = async(req: Request, res: Response, next: NextFu
 };
 
 export const getAllCommands = async(req: Request, res: Response, next: NextFunction) => {
-    const itemID: string = req.params.id;
-    const result = await axios.get(APP_API_IP + 'commands/' + itemID, {
+    const result = await axios.get(APP_API_IP + 'commands/', {
         headers:req.headers
     })
         .then(response => response.data)
