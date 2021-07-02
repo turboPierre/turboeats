8398cac03d41bdf030293ce0c291f7a7a90dbe8e
turboPierre <pierre.defert@viacesi.fr>
2021-07-02 09:46:36 +0200

    Documentation fixed

--- resolution_controller/public/docs/api_project.js
+++ resolution_controller/public/docs/api_project.js
@@ -3,13 +3,13 @@ define({
   "version": "0.1.0",
   "description": "Informations Application API",
   "title": "Documentation",
-  "url": "http://localhost:8082/",
+  "url": "http://localhost:8082",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-07-02T07:31:03.863Z",
+    "time": "2021-07-02T07:44:58.845Z",
     "url": "https://apidocjs.com",
     "version": "0.28.1"
   }
